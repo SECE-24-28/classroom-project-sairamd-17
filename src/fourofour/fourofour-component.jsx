92cdb23bff0934983541e9419a81be2819c2f3fc
sairam <sairamdpm81@gmail.com>
2025-12-08 14:27:32 +0530

    added many new files  usestate and props practices files

---
+++ src/fourofour/fourofour-component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const FourOFourcomponent = () => {
+  return (
+    <div>
+      <h1>There is an error!</h1>
+      <img 
+        id="d" 
+        alt="Dogs of Amazon" 
+        src="D:\Mern_intern\classroom-project-sairamd-17\src\assets\WhatsApp Image 2025-12-08 at 12.07.42_a4f5b9f0.jpg"
+      />
+    </div>
+  );
+};
+export default FourOFourcomponent;
---
+++ src/props-practice/prop-component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const PropsPractice = (props) =>{
+    return(
+        <>
+        <h1>Name</h1>
+        <div> Name :{props.name}</div>
+        </> 
+
+    );
+};
+export default PropsPractice;
